09d31d16de92b76e3b190ed020eb15d9c10b1121
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-12-24 21:19:37 -0800

    Update PacketInfo.php

--- src/Eren5960/PacketInfo/PacketInfo.php
+++ src/Eren5960/PacketInfo/PacketInfo.php
@@ -21,24 +21,24 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\Task;
 use pocketmine\utils\Terminal;
 
-class PacketInfo extends PluginBase implements Listener{
+class PacketInfo extends PluginBase implements Listener {
 
     /** @var string[] */
     private $not = [];
     /** @var int */
     private $count = 0;
 
-    public function onLoad(){
+    public function onLoad() {
         $this->saveDefaultConfig();
     }
 
-    public function onEnable(): void{
+    public function onEnable(): void {
         $this->not = $this->getConfig()->get("no-send-info-packets", []);
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getScheduler()->scheduleRepeatingTask(new class($this) extends Task{private $plugin;public function __construct(PacketInfo $plugin){$this->plugin = $plugin;}public function onRun(int $currentTick){$this->plugin->sendReceivedPacketCount();}}, 1200);
     }
 
-    public function onReceivePacket(DataPacketReceiveEvent $event){
+    public function onReceivePacket(DataPacketReceiveEvent $event) {
         $packet = $event->getPacket();
         if ($this->canSend($packet)) {
             $this->sendPacketInfo($packet, $event->getPlayer());
@@ -50,7 +50,7 @@ class PacketInfo extends PluginBase implements Listener{
      * @param DataPacket $packet
      * @return bool
      */
-    private function canSend(DataPacket $packet): bool{
+    private function canSend(DataPacket $packet): bool {
         return !in_array($packet->getName(), $this->not) && $this->getConfig()->get("send-packet-info");
     }
 
@@ -58,11 +58,11 @@ class PacketInfo extends PluginBase implements Listener{
      * @param DataPacket $packet
      * @param Player $player
      */
-    private function sendPacketInfo(DataPacket $packet, Player $player): void{
+    private function sendPacketInfo(DataPacket $packet, Player $player): void {
         echo Terminal::$COLOR_AQUA . "Received packet: " . Terminal::$COLOR_GOLD . $packet->getName() . Terminal::$COLOR_AQUA . " from " . Terminal::$COLOR_GREEN . $player->getName() . "\n";
     }
 
-    public function sendReceivedPacketCount(): void{
+    public function sendReceivedPacketCount(): void {
         echo Terminal::$COLOR_AQUA . "Received packet count in 1 minute: " . Terminal::$COLOR_GOLD . $this->count . "\n";
         $this->count = 0;
     }
